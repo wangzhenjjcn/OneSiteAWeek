c8afb9af41913f4d240939368157374d24024665
WangZhen <wangzhenjjcn@gmail.com>
2024-02-01 05:00:22 +0800

    Update 编译APP.bat

--- System/src/编译APP.bat
+++ System/src/编译APP.bat
@@ -1,8 +1,10 @@
 set "SCRIPT_DIR=%~dp0"
 pip3 install --upgrade pip
-pip3 install  -r requirement.txt
-pip3 install  -r requirements.txt
+
 pip3 install pyinstaller -i https://pypi.tuna.tsinghua.edu.cn/simple
-PyInstaller -y -F -w -n YaTuDownLoader --icon=favicon.gif --additional-hooks=extra-hooks --additional-hooks-dir "%SCRIPT_DIR%\BIn" %SCRIPT_DIR%\app.py 
+
+rd /s /q "%SCRIPT_DIR%\Bin\Logs"
+PyInstaller -y -F -n App --add-data "%SCRIPT_DIR%\Bin;./Bin" %SCRIPT_DIR%\m3u8Downloader.py 
+
 @pause
  
